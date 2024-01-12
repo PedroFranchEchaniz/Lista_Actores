e643a9cf39b73acd1f5b4acde1aa42c301e1b2af
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2024-01-12 10:53:29 +0100

    lista de actores

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/widget/actor_item.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 60.0),
+        child: Column(
+          children: [
+            const Text('Actores Famosos'),
+            SizedBox(
+              height: 500,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  ActorItem(
+                    nombreActor: 'Paco',
+                    url:
+                        'https://resizing.flixster.com/xtnsozkTuUtYkGMETMLMJfgNTjs=/218x280/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/47162_v9_bc.jpg',
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/actor_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ActorItem extends StatelessWidget {
+  final String nombreActor;
+  final String url;
+
+  const ActorItem({
+    Key? key,
+    required this.nombreActor,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            margin: EdgeInsets.only(top: 80.0, left: 20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50.0),
+              image: DecorationImage(
+                image: NetworkImage(url),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              nombreActor,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
